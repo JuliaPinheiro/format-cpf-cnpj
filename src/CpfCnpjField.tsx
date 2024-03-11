cffddab826df361ddfd14c2dc48c920feea0060c
Julia Pinheiro <julia.pinheiro@libercapital.com.br>
2024-03-11 11:52:46 -0300

    Atualizar dependências do @mui/material e importação do TextField

--- src/CpfCnpjField.tsx
+++ src/CpfCnpjField.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
 import { formatCpfCnpj } from './formatCpfCnpj';
 import { Props } from './types';
+import { TextField } from '@mui/material';
 
 export const CpfCnpjField: React.FC<Props> = ({
   value,
